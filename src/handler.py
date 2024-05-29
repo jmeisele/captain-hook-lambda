0998ae1f4a329d907c55305d981b5088281b3283
jmeisele <jmeisele@yahoo.com>
2024-05-29 19:04:24 +0000

    :construction:

--- src/handler.py
+++ src/handler.py
@@ -41,7 +41,7 @@ def lambda_handler(event: Dict, context: Dict) -> Dict[str, Any]:
         return {
             "statusCode": 400,
             "headers": {"Content-Type": "application/json"},
-            "body": json.dumps({"error": e}),
+            "body": json.dumps({"error": e.errors()}),
         }
 
     # If repo created event proceed, otherwise skip
